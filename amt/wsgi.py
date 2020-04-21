5d6e73f44831b81ca23ea145f952a084e86dd2a8
Laur04 <pmd7211@gmail.com>
2020-04-21 19:34:52 -0400

    update settings

--- amt/wsgi.py
+++ amt/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amt.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'amt.settings'
 
 application = get_wsgi_application()
