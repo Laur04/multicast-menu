be1a68ad3906c403e4497eb155d8eb8e4cb7d457
lauren <pmd7211@gmail.com>
2021-11-17 10:23:53 -0500

    add debug statements

--- multicast/apps/add/tasks.py
+++ multicast/apps/add/tasks.py
@@ -14,8 +14,12 @@ def submit_file_to_translator(submission_id):
     submission.active = True
     submission.save()
 
+    print(submission.id)
+
     cmd = "/usr/bin/sudo -u web /usr/bin/vlc {} --sout=udp://162.250.138.11:9001 --loop --sout-keep".format(submission.path_to_uploaded_file)
     os.system(cmd)
+
+    print("ran command")
 
     while True:
         time.sleep(100)
