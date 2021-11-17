136e6163af13f280e04cac5beeddb6a318576d55
lauren <pmd7211@gmail.com>
2021-11-17 10:13:55 -0500

    remove exceptions

--- multicast/apps/add/tasks.py
+++ multicast/apps/add/tasks.py
@@ -9,46 +9,37 @@ from .models import ManualReport, StreamSubmission
 # Streams the file out to the translator
 @shared_task
 def submit_file_to_translator(submission_id):
-    try:
-        submission = StreamSubmission.objects.get(id=submission_id)
-        submission.celery_task_id = current_task.task_id
-        submission.active = True
-        submission.save()
+    submission = StreamSubmission.objects.get(id=submission_id)
+    submission.celery_task_id = current_task.task_id
+    submission.active = True
+    submission.save()
 
-        cmd = "/usr/bin/sudo -u web /usr/bin/vlc {} --sout=udp://162.250.138.11:9001 --loop --sout-keep".format(submission.path_to_uploaded_file)
-        os.system(cmd)
+    cmd = "/usr/bin/sudo -u web /usr/bin/vlc {} --sout=udp://162.250.138.11:9001 --loop --sout-keep".format(submission.path_to_uploaded_file)
+    os.system(cmd)
 
-        while True:
-            time.sleep(100)
-    except:
-        pass
+    while True:
+        time.sleep(100)
 
 
 # Recieves the live content from the URL and streams it out to the translator
 @shared_task
 def submit_link_to_translator(submission_id):
-    try:
-        submission = StreamSubmission.objects.get(id=submission_id)
-        submission.celery_task_id = current_task.task_id
-        submission.active = True
-        submission.save()
+    submission = StreamSubmission.objects.get(id=submission_id)
+    submission.celery_task_id = current_task.task_id
+    submission.active = True
+    submission.save()
 
-        cmd = "/usr/bin/sudo -u web /usr/bin/vlc {} --sout=udp://162.250.138.11:9001 --loop --sout-keep".format(submission.path_to_uploaded_file)
-        os.system(cmd)
+    cmd = "/usr/bin/sudo -u web /usr/bin/vlc {} --sout=udp://162.250.138.11:9001 --loop --sout-keep".format(submission.path_to_uploaded_file)
+    os.system(cmd)
 
-        while True:
-            time.sleep(100)
-    except:
-        pass
+    while True:
+        time.sleep(100)
 
 
 # Verifies the stream being reported before adding it
 @shared_task
 def verify_manual_report(report_id):
-    try:
-        report = ManualReport.objects.get(id=report_id)
-        report.celery_task_id = current_task.task_id
-        report.active = True
-        report.save()
-    except:
-        pass
+    report = ManualReport.objects.get(id=report_id)
+    report.celery_task_id = current_task.task_id
+    report.active = True
+    report.save()
