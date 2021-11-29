1f75d05d304623efa1474821a513ee2782b564c9
lauren <pmd7211@gmail.com>
2021-11-29 15:08:03 -0500

    streaming collection fixes 2

--- multicast/apps/add/admin.py
+++ multicast/apps/add/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import ManualReport, StreamSubmission
+from .models import FailedQuery, ManualReport, StreamSubmission
 
 
+admin.site.register(FailedQuery)
 admin.site.register(ManualReport)
 admin.site.register(StreamSubmission)
