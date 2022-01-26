cc6e4a5e016712a52a0d25174e554f8fb203c51f
lauren <pmd7211@gmail.com>
2022-01-26 09:27:01 -0500

    update vlc destination

--- multicast/apps/add/tasks.py
+++ multicast/apps/add/tasks.py
@@ -14,7 +14,7 @@ def submit_file_to_translator(submission_id):
     submission.active = True
     submission.save()
 
-    proc = subprocess.Popen(["/usr/bin/sudo", "-u", "web", "/usr/bin/vlc", submission.path_to_uploaded_file, "--sout=udp://162.250.138.11:9001", "--loop", "--sout-keep"])
+    proc = subprocess.Popen(["/usr/bin/sudo", "-u", "web", "/snap/bin/vlc", submission.path_to_uploaded_file, "--sout=udp://162.250.138.11:9001", "--loop", "--sout-keep"])
     submission.task_pid = proc.pid
     submission.save()
 
