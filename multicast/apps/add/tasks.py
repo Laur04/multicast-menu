aebd0560f0cccad49cf3ced56a73b3e1bb192620
lauren <pmd7211@gmail.com>
2022-04-09 14:30:07 -0400

    fix

--- multicast/apps/add/tasks.py
+++ multicast/apps/add/tasks.py
@@ -40,7 +40,7 @@ def submit_file_to_translator(stream_id):
     upload.active = True
     upload.save()
 
-    proc = subprocess.Popen(["/usr/bin/sudo", "-u", "web", "/usr/bin/vlc", upload.path_to_uploaded_file, "--sout=udp://162.250.138.11:9001", "--loop", "--sout-keep"])
+    proc = subprocess.Popen(["/usr/bin/sudo", "-u", "web", "/usr/bin/vlc", upload.uploaded_file, "--sout=udp://162.250.138.11:9001", "--loop", "--sout-keep"])
     upload.task_pid = proc.pid
     upload.save()
 
